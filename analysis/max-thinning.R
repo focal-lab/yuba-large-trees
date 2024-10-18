eafc368ffbb05df2543c5da342983c0a7d1e47d6
Emily Marie Purvis <emily.m.purvis@ucdavis.edu>
2024-10-17 21:17:26 -0700

    + plots. the violin plots look silly but the last two histograms (combo all densities/all bas) are good.

--- analysis/max-thinning.R
+++ analysis/max-thinning.R
@@ -5,6 +5,7 @@
 #### Load libraries ####
 
 library(tidyverse)
+library(ggplot2)
 library(stringr)
 
 #### Import data ####
@@ -336,4 +337,187 @@ for(i in 1:nrow(trees)) {
 
 # export 30 and 40 inch blanket rules
 
-write.csv(plot_summaries, "C:\\Users\\emily\\Desktop\\TNC Yuba\\data\\IRI data\\plot_summaries_thinned_demos.csv")
+# write.csv(plot_summaries, "C:\\Users\\emily\\Desktop\\TNC Yuba\\data\\IRI data\\plot_summaries_thinned_demos.csv")
+
+#### Graph results ####
+
+plot_summaries$unthinned_ba_m2ha <- as.numeric(plot_summaries$unthinned_ba_m2ha)
+
+plot_summaries$unthinned_density_tph <- as.numeric(plot_summaries$unthinned_density_tph)
+
+plot_summaries$thinned_ba_m2ha_30in <- as.numeric(plot_summaries$thinned_ba_m2ha_30in)
+
+plot_summaries$thinned_density_tph_30in <- as.numeric(plot_summaries$thinned_density_tph_30in)
+
+plot_summaries$thinned_ba_m2ha_40in <- as.numeric(plot_summaries$thinned_ba_m2ha_40in)
+
+plot_summaries$thinned_density_tph_40in <- as.numeric(plot_summaries$thinned_density_tph_40in)
+
+# make a subset of plot_summaries that only has mixed conifer plots
+
+plot_summaries_mixedconifer <- subset (plot_summaries, mixed_conifer == TRUE)
+
+#### Violin plots ####
+
+# simple 1 variable density violin plots
+
+violin_unthinned_density <- 
+  ggplot(plot_summaries_mixedconifer, aes(x="", y=unthinned_density_tph)) +
+  ylim(0,850) +
+  labs(title="Unthinned plot density", x="", y="density_tph") +
+  geom_violin() + 
+  geom_boxplot(width=0.1) +
+  theme_classic()
+
+violin_unthinned_density
+
+violin_thinned_density_30 <- 
+  ggplot(plot_summaries_mixedconifer, aes(x="", y=thinned_density_tph_30in)) +
+  ylim(0,130) +
+  labs(title="Thinned plot density 30 inch rule", x="", y="density_tph") +
+  geom_violin() + 
+  geom_boxplot(width=0.1) +
+  theme_classic()
+
+violin_thinned_density_30
+
+# make 2 column data frame to enable creation of side by side violin plots comparing thinning scenarios and unthinned plots
+
+plot_densities <- plot_summaries_mixedconifer[,-(1:2)]
+plot_densities <- plot_densities[,-(2:3)]
+plot_densities <- plot_densities[,-(3)]
+plot_densities <- plot_densities[,-(4)]
+
+plot_density_unthinned <- as.data.frame(plot_densities[,-(2:3)])
+plot_density_unthinned$thinning_scenario <- "unthinned"
+plot_density_unthinned <- plot_density_unthinned %>% rename("density_tph" = 1)
+
+plot_density_30 <- plot_densities[,-(1)]
+plot_density_30$thinning_scenario <- "30 inch rule"
+plot_density_30 <- plot_density_30[,-(2)]
+plot_density_30 <- plot_density_30 %>% rename("density_tph" = 1)
+
+plot_density_40 <- as.data.frame(plot_densities[,-(1:2)])
+plot_density_40$thinning_scenario <- "40 inch rule"
+plot_density_40 <- plot_density_40 %>% rename("density_tph" = 1)
+
+plot_density_2column <- full_join(plot_density_30, plot_density_40)
+plot_density_2column <- full_join(plot_density_2column, plot_density_unthinned)
+
+# violin plot with all densities
+
+violin_all_densities <- 
+  ggplot(plot_density_2column, aes(x=thinning_scenario, y=density_tph, fill = thinning_scenario)) +
+  ylim(0,700) +
+  labs(title="Plot Densities", x="Thinning scenario", y="Density (trees per hectare)") +
+  geom_violin() + 
+  geom_boxplot(width=0.01) +
+  theme_classic()
+
+violin_all_densities
+
+# violin plot with just 30 and 40 inch rule densities
+
+plot_density_2column_3040 <- full_join(plot_density_30, plot_density_40)
+
+violin_density_thinned <- 
+  ggplot(plot_density_2column_3040, aes(x=thinning_scenario, y=density_tph, fill = thinning_scenario)) +
+  ylim(0,130) +
+  labs(title="Plot Densities", x="Thinning scenario", y="Density (trees per hectare)") +
+  geom_violin() + 
+  geom_boxplot(width=0.05) +
+  theme_classic()
+
+violin_density_thinned
+
+# simple 1 variable ba violin plots
+
+violin_unthinned_ba <- 
+  ggplot(plot_summaries_mixedconifer, aes(x="", y=unthinned_ba_m2ha)) +
+ # ylim(0,1600) +
+  labs(title="Unthinned plot basal area", x="", y="ba_m2ha") +
+  geom_violin() + 
+  geom_boxplot(width=0.1) +
+  theme_classic()
+
+violin_unthinned_ba
+
+# make 2 column BA data frame to enable creation of side by side violin plots comparing thinning scenarios and unthinned plots
+
+plot_bas <- plot_summaries_mixedconifer[,-(1)]
+plot_bas <- plot_bas[,-(2:5)]
+plot_bas <- plot_bas[,-(3)]
+
+plot_ba_unthinned <- as.data.frame(plot_bas[,-(2:3)])
+plot_ba_unthinned$thinning_scenario <- "unthinned"
+plot_ba_unthinned <- plot_ba_unthinned %>% rename("ba_m2ha" = 1)
+
+plot_ba_30 <- plot_bas[,-1]
+plot_ba_30$thinning_scenario <- "30 inch rule"
+plot_ba_30 <- plot_ba_30[,-2]
+plot_ba_30 <- plot_ba_30 %>% rename("ba_m2ha" = 1)
+
+plot_ba_40 <- as.data.frame(plot_bas[,-(1:2)])
+plot_ba_40$thinning_scenario <- "40 inch rule"
+plot_ba_40 <- plot_ba_40 %>% rename("ba_m2ha" = 1)
+
+plot_bas_2column <- full_join(plot_ba_30, plot_ba_40)
+plot_bas_2column <- full_join(plot_bas_2column, plot_ba_unthinned)
+
+# violin plot with all BAs
+
+violin_all_bas <- 
+  ggplot(plot_bas_2column, aes(x=thinning_scenario, y=ba_m2ha, fill = thinning_scenario)) +
+#  ylim(0,850) +
+  labs(title="Plot basal areas", x="Thinning scenario", y="Basal area (m2 per ha)") +
+  geom_violin() + 
+  geom_boxplot(width=0.01) +
+  theme_classic()
+
+violin_all_bas
+
+#### Histograms ####
+
+# unthinned density
+
+histo_unthinned_density <-
+  ggplot(plot_summaries_mixedconifer, aes(x=unthinned_density_tph, y = after_stat(density))) + 
+  geom_histogram(binwidth=25, , colour="black", fill="white") +
+  labs(title="Unthinned plot densities", x="Unthinned density (tph)", y="Number of plots") +
+  geom_density(alpha=.25) 
+
+histo_unthinned_density
+
+# the kernal density estimate looks dumb
+
+histo_unthinned_density <-
+  ggplot(plot_summaries_mixedconifer, aes(x=unthinned_density_tph)) + 
+  geom_histogram(binwidth=25, , colour="black", fill="white") +
+  labs(title="Unthinned plot densities", x="Unthinned density (tph)", y="Number of plots") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+histo_unthinned_density
+
+# histogram with the unthinned + thinned densities
+
+histo_all_densities <-
+  ggplot(plot_density_2column, aes(x=density_tph, color = thinning_scenario)) + 
+  geom_histogram(binwidth=10, fill="white", alpha = 0.5, position = "dodge") +
+  labs(title="Plot densities", x="Density (tph)", y="Number of plots") +
+  guides(color = guide_legend(title = "Thinning scenario")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_brewer(palette="Set2")
+
+histo_all_densities
+
+# histogram with unthinned + thinned BAs
+
+histo_all_bas <-
+  ggplot(plot_bas_2column, aes(x=ba_m2ha, color = thinning_scenario)) + 
+  geom_histogram(binwidth=25, fill="white", alpha = 0.5, position = "dodge") +
+  labs(title="Plot basal areas", x="Basal area (m2/ha)", y="Number of plots") +
+  guides(color = guide_legend(title = "Thinning scenario")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_brewer(palette="Set2")
+
+histo_all_bas
